acd7621c7066210a3e65eb97ab86bd64288583e3
Gabriel Zambrano <gab.zambrano@gmail.com>
2025-04-02 17:51:02 +0200

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
+// Import tailwind correctly
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gabzon.github.io',
-  base: '/botanist',
+  base: 'botanist',
+  trailingSlash: "never",
   output: 'static',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
