565c29d9379b99355e8d8a320f167c9e194093c3
Gabriel Zambrano <gab.zambrano@gmail.com>
2025-04-02 14:15:22 +0200

    Fix base path in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gabzon.github.io',
-  base: 'botanist',
+  base: '/botanist',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
